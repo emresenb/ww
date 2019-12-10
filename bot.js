9f3ae40684ff1571b572a33ab335bbbdfb14fb8f
Glitch (tartan-rain) <none>
2019-12-10 23:34:18 +0000

    👫🍪 Checkpoint ./bot.js:8735722/10

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ const app = express();
 app.get("/", (request, response) => {
   response.sendStatus(200);
     client.user.setStatus("dnd");
-  client.user.setActivity(`!yardım | Sizi`, { type: "LISTENING"});
+  client.user.setActivity(`!play | Sizi`, { type: "LISTENING"});
 });
 app.listen(process.env.PORT);
 setInterval(() => {
