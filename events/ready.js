3c6aed9f4741ee91931133a53e3537daeca8fffe
Glitch (tartan-rain) <none>
2020-02-18 15:16:11 +0000

    🙈🏤 Checkpoint ./events/ready.js:9181222/687

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("watching");
-  client.user.setGame(`Bot Aktif`);
+  client.user.setGame(`Yapımcım: ►EmreHD35◄#5726`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`Bot Aktif`);
+  console.log(`Yapımcım: ►EmreHD35◄#5726`);
 };
