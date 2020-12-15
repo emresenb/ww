ad620b4d1da706ed18794a5879b13b7933ed5fe4
Glitch (tartan-rain) <none>
2020-12-15 13:22:21 +0000

    🐚✌️ Checkpoint ./events/ready.js:26224968/101 ./ayarlar.json:26224968/18

--- events/ready.js
+++ events/ready.js
@@ -8,8 +8,8 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("watching");
-  client.user.setGame(`Yapımcım: ►EmreHD35◄#5726`);
+  client.user.setStatus("playing);
+  client.user.setGame(`PUKE RADYO BOTU `);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`Yapımcım: ►EmreHD35◄#5726`);
 };
